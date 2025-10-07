a34b2c78807e2c3d1fb740eee2977b20e71cbd16
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-10-07 10:07:43 -0700

    Tweaked defaults

--- src/editor.rs
+++ src/editor.rs
@@ -202,7 +202,7 @@ impl Default for RenderSettings {
             right_hue: 328.0,
             minimum_lightness: 0.13,
             min_db: -70.0,
-            max_db: -9.0,
+            max_db: -12.0,
             bargraph_height: 0.4,
             spectrogram_duration: Duration::from_secs_f64(0.5),
             show_performance: true,
