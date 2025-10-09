3aa83bf2d258cac46400739cb777428ef787e684
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-10-09 09:43:21 -0700

    Tweaked defaults

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -26,7 +26,7 @@ impl Default for BetterAnalyzerConfiguration {
             erb_frequency_scale: true,
             sample_rate: 48000,
             time_resolution: 37.0,
-            erb_time_resolution: false,
+            erb_time_resolution: true,
             erb_min_time_resolution: 20.0,
             nc_method: true,
         }
