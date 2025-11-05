743a927493557a7188a71ec75e27f0b796a79f3f
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-05 09:07:55 -0800

    Added value normalization

--- osc-handler.rs
+++ osc-handler.rs
@@ -17,53 +17,80 @@ use clap::Parser;
 use rosc::{OscArray, OscBundle, OscMessage, OscPacket, OscTime, OscType, decoder, encoder};
 
 use std::{
+    collections::VecDeque,
     net::{SocketAddr, UdpSocket},
     time::Duration,
 };
 
-struct Handler {}
+struct Handler {
+    normalization_data: VecDeque<(f64, Duration)>,
+    normalization_duration: Duration,
+    agc_length: Duration,
+    above_masking: f64,
+    below_masking: f64,
+}
 
 impl Handler {
-    fn new() -> Self {
-        Self {}
+    fn new(config: &Args) -> Self {
+        Self {
+            normalization_data: VecDeque::with_capacity(8192),
+            normalization_duration: Duration::ZERO,
+            agc_length: Duration::from_secs_f32(config.agc_length),
+            above_masking: config.above_masking as f64,
+            below_masking: config.below_masking as f64,
+        }
+    }
+    fn get_normalization_targets(&mut self, masking_mean: f32, duration: Duration) -> (f32, f32) {
+        self.normalization_data
+            .push_back((masking_mean as f64, duration));
+        self.normalization_duration += duration;
+
+        while self.normalization_duration > self.agc_length {
+            let front = self.normalization_data.pop_front().unwrap();
+            self.normalization_duration -= front.1;
+        }
+
+        let mean = self
+            .normalization_data
+            .iter()
+            .map(|(d, l)| d * l.as_secs_f64())
+            .sum::<f64>()
+            / self.normalization_duration.as_secs_f64();
+
+        (
+            (mean - self.below_masking) as f32,
+            (mean + self.above_masking) as f32,
+        )
     }
     fn handle_packet(&mut self, packet: OscPacket) -> anyhow::Result<Vec<OscPacket>> {
         let data = VisualizerData::try_from(packet).map_err(|e| anyhow::anyhow!(e))?;
+
+        let (lower, upper) = self.get_normalization_targets(data.masking_mean, data.duration);
 
         // Magnitude should range from 0 to 1
         // Buttons should be either float(1) or float(0)
 
         Ok(vec![OscPacket::Bundle(OscBundle {
             timetag: data.timetag,
-            content: vec![
-                OscPacket::Message(OscMessage {
-                    addr: "/katvisualizer/stats".to_string(),
-                    args: vec![
-                        OscType::Float(data.masking_mean),
-                        OscType::Float(data.mean),
-                        OscType::Float(data.max),
-                    ],
-                }),
-                OscPacket::Message(OscMessage {
-                    addr: "/katvisualizer/tones".to_string(),
-                    args: vec![OscType::Array(OscArray {
-                        content: data
-                            .analysis
-                            .into_iter()
-                            .map(|(f, b, _, v, stm)| {
-                                OscType::Array(OscArray {
-                                    content: vec![
-                                        OscType::Float(f),
-                                        OscType::Float(b),
-                                        OscType::Float(v),
-                                        OscType::Float(stm),
-                                    ],
-                                })
+            content: vec![OscPacket::Message(OscMessage {
+                addr: "/tones".to_string(),
+                args: vec![OscType::Array(OscArray {
+                    content: data
+                        .analysis
+                        .into_iter()
+                        .map(|(f, _b, _p, v, _stm)| {
+                            OscType::Array(OscArray {
+                                content: vec![
+                                    OscType::Float(f),
+                                    OscType::Float(
+                                        map_value_f32(v, lower, upper, 0.0, 1.0).clamp(0.0, 1.0),
+                                    ),
+                                ],
                             })
-                            .collect(),
-                    })],
-                }),
-            ],
+                        })
+                        .collect(),
+                })],
+            })],
         })])
     }
 }
@@ -76,6 +103,68 @@ struct VisualizerData {
     mean: f32,
     max: f32,
     analysis: Vec<(f32, f32, f32, f32, f32)>,
+}
+
+/// A server for converting KatVisualizer's OSC output into a more usable format.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The UDP/IP address that the server should listen on for OSC packets from KatVisualizer.
+    #[arg(short, long)]
+    listen_address: SocketAddr,
+
+    /// The UDP/IP address that converted OSC packets should be sent to.
+    #[arg(short, long)]
+    destination_address: SocketAddr,
+
+    #[arg(long, default_value_t = 1.0)]
+    agc_length: f32,
+
+    #[arg(long, default_value_t = 30.0)]
+    above_masking: f32,
+
+    #[arg(long, default_value_t = 0.0)]
+    below_masking: f32,
+}
+
+const MAX_PACKET_SIZE: usize = 65535;
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let socket = UdpSocket::bind(args.listen_address)?;
+    let mut read_buf = [0; MAX_PACKET_SIZE];
+    let mut write_buf = Vec::with_capacity(MAX_PACKET_SIZE);
+
+    let mut handler = Handler::new(&args);
+
+    println!(
+        "Listening address: {}\nDestination address: {}",
+        args.listen_address, args.destination_address
+    );
+
+    loop {
+        let length = socket.recv(&mut read_buf)?;
+        let received = &read_buf[0..length];
+
+        match decoder::decode_udp(received) {
+            Ok((_, packet)) => match handler.handle_packet(packet) {
+                Ok(responses) => {
+                    for response in responses {
+                        write_buf.clear();
+                        encoder::encode_into(&response, &mut write_buf)?;
+                        socket.send_to(&write_buf, args.destination_address)?;
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Failed to handle OSC packet:\n{:#?}", e);
+                }
+            },
+            Err(e) => {
+                eprintln!("Failed to decode packet:\n{:#?}", e);
+            }
+        }
+    }
 }
 
 const FORMAT_VERSION: &str = "v0.8.4";
@@ -193,55 +282,6 @@ impl TryFrom<OscPacket> for VisualizerData {
     }
 }
 
-/// A server for converting KatVisualizer's OSC output into a more usable format.
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// The UDP/IP address that the server should listen on for OSC packets from KatVisualizer.
-    #[arg(short, long)]
-    listen_address: SocketAddr,
-
-    /// The UDP/IP address that converted OSC packets should be sent to.
-    #[arg(short, long)]
-    destination_address: SocketAddr,
-}
-
-const MAX_PACKET_SIZE: usize = 65535;
-
-fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
-
-    let socket = UdpSocket::bind(args.listen_address)?;
-    let mut read_buf = [0; MAX_PACKET_SIZE];
-    let mut write_buf = Vec::with_capacity(MAX_PACKET_SIZE);
-
-    let mut handler = Handler::new();
-
-    println!(
-        "Listening address: {}\nDestination address: {}",
-        args.listen_address, args.destination_address
-    );
-
-    loop {
-        let length = socket.recv(&mut read_buf)?;
-        let received = &read_buf[0..length];
-
-        match decoder::decode_udp(received) {
-            Ok((_, packet)) => match handler.handle_packet(packet) {
-                Ok(responses) => {
-                    for response in responses {
-                        write_buf.clear();
-                        encoder::encode_into(&response, &mut write_buf)?;
-                        socket.send_to(&write_buf, args.destination_address)?;
-                    }
-                }
-                Err(e) => {
-                    eprintln!("Failed to handle OSC packet:\n{:#?}", e);
-                }
-            },
-            Err(e) => {
-                eprintln!("Failed to decode packet:\n{:#?}", e);
-            }
-        }
-    }
+pub fn map_value_f32(x: f32, min: f32, max: f32, target_min: f32, target_max: f32) -> f32 {
+    (x - min) / (max - min) * (target_max - target_min) + target_min
 }
