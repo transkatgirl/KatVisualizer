4e092da2eb353298bc87aaf2a2010360bfe04816
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-06 23:03:33 -0800

    Tweaked parameters

--- osc-scripts/osc-handler.rs
+++ osc-scripts/osc-handler.rs
@@ -258,7 +258,7 @@ struct Args {
     #[arg(long, default_value_t = 15.0)]
     agc_target_minimum: f32,
 
-    #[arg(long, default_value_t = 30.0)]
+    #[arg(long, default_value_t = 36.0)]
     above_masking: f32,
 
     #[arg(long, default_value_t = -10.0)]
