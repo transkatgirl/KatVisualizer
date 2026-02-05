292845956197fbc8343ccd4fb4162dca16daef90
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-02-05 12:24:31 -0800

    Updated version to 0.14.0

--- osc-scripts/osc-handler.rs
+++ osc-scripts/osc-handler.rs
@@ -349,7 +349,7 @@ fn main() -> anyhow::Result<()> {
     }
 }
 
-const FORMAT_VERSION: &str = "v0.12.18";
+const FORMAT_VERSION: &str = "v0.14.0";
 
 impl TryFrom<OscPacket> for VisualizerData {
     type Error = &'static str;
